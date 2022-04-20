bee6c65115ba5f1398d9e6f82a3fa99d4647aa65
Robeljone <robelict94@gmail.com>
2022-04-20 20:28:02 +0300

    commited

--- admin/thread.php
+++ admin/thread.php
@@ -6,23 +6,18 @@ $timezone = new DateTimeZone('Africa/Nairobi');
 	$d2 = new Datetime("now", $timezone);
 	$schdate = $d2->format('Y-m-d');
 	$time = $d2->format('H:i:s');
-	$qry = $conn->query("SELECT * from employee_schedule where dates='$schdate' ");
+	$qry = $conn->query("SELECT emp_id,employee_name from employee_schedule where dates='$schdate' ");
     $row = mysqli_fetch_array($qry);
 	if($qry->num_rows > 0)
     {
-        //echo $row;
-        foreach($row as $data)
+        $data = array();
+        foreach($row as $key=> $data)
         {
-           $qry2 = $conn->query("SELECT * FROM attendance where employee_id='".$row["emp_id"]."' and  datetime_log='$schdate' and log_type='clock_in' ");
+           $qry2 = $conn->query("SELECT * FROM attendance where Date(datetime_log)='$schdate' and log_type='Clock-in' and employee_id='".$data[0]."' ");
             if($qry2->num_rows < 0)
             {
-                echo "test done";
-                //$save_log= $conn->query("INSERT INTO attendance (log_type,employee_id,employee_name,status) values (' ','".$row['emp_id']."','".$row['employee_name']."','Absent')");
+        $save_log= $conn->query("INSERT INTO attendance (log_type,employee_id,employee_name,statu) values (' ','".$data[0]."','".$data[0]."','Absent')");
             } 
-            else
-            {
-                
-            }
         }
         // while($row!=null)
         // {
